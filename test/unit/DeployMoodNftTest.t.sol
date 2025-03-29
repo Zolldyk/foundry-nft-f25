3fed0eb937b11122e93dde576b125b103b148158
Zolldyck <buadesoye@gmail.com>
2025-03-30 00:35:17 +0100

    Nft updates

---
+++ test/unit/DeployMoodNftTest.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {DeployMoodNft} from "../../script/DeployMoodNft.s.sol";
+
+contract DeployMoodNftTest is Test {
+    DeployMoodNft public deployer;
+
+    function setUp() public {
+        deployer = new DeployMoodNft();
+    }
+
+    function testConvertSvgToUri() public view {
+        string
+            memory expectedUri = "data:image/svg+xml;base64,PHN2ZyB2aWV3Qm94PSIwIDAgMjAwIDIwMCIgd2lkdGg9IjQwMCIgIGhlaWdodD0iNDAwIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPgogIDxjaXJjbGUgY3g9IjEwMCIgY3k9IjEwMCIgZmlsbD0ieWVsbG93IiByPSI3OCIgc3Ryb2tlPSJibGFjayIgc3Ryb2tlLXdpZHRoPSIzIi8+CiAgPGcgY2xhc3M9ImV5ZXMiPgogICAgPGNpcmNsZSBjeD0iNzAiIGN5PSI4MiIgcj0iMTIiLz4KICAgIDxjaXJjbGUgY3g9IjEyNyIgY3k9IjgyIiByPSIxMiIvPgogIDwvZz4KICA8cGF0aCBkPSJtMTM2LjgxIDExNi41M2MuNjkgMjYuMTctNjQuMTEgNDItODEuNTItLjczIiBzdHlsZT0iZmlsbDpub25lOyBzdHJva2U6IGJsYWNrOyBzdHJva2Utd2lkdGg6IDM7Ii8+Cjwvc3ZnPg==";
+        string
+            memory svg = '<svg xmlns="http://www.w3.org/2000/svg" width="500" height="500"><text x="0" y="15" fill="black">Hi! Your browser decoded this</text></svg>';
+        string memory actualUri = deployer.svgToImageURI(svg);
+        assert(
+            keccak256(abi.encodePacked(actualUri)) ==
+                keccak256(abi.encodePacked(expectedUri))
+        );
+    }
+}
